c16524467151fac93e17e49192159c414230d3cb
Love Hasija <love2dishtech@gmail.com>
2017-06-05 10:09:37 +0530

    Added the Scalaxb SerDe example.

---
+++ scalaxb-serde/build.sbt
@@ -0,0 +1,6 @@
+name := "scalaxb-examples"
+version := "1.0"
+scalaVersion := "2.11.8"
+
+libraryDependencies +=  "org.scala-lang" % "scala-xml" % "2.11.0-M4"
+libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.6"
---
+++ scalaxb-serde/src/main/scala/com/codersbistro/soapexample/generated/Main.scala
@@ -0,0 +1,27 @@
+package com.codersbistro.soapexample.generated
+
+/**
+  * Created by loveh on 6/4/2017.
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+    /*Example showing conversion of scala case classes to XML*/
+    val addressXML =
+      <shipTo xmlns="http://www.example.com/IPO">
+        <name>Foo</name>
+        <street>1537 Paper Street</street>
+        <city>Wilmington</city>
+      </shipTo>
+    val parsedAddress = scalaxb.fromXML[Address](addressXML)
+    println(s"Rendered XML case class is :: ${parsedAddress}")
+
+    /*Example showing conversion of scala case classes to XML*/
+    val newAddress = Address(
+      name = "221B",
+      street = "Bakers Street",
+      city = "London"
+    )
+    val renderedXML = scalaxb.toXML[Address](newAddress, "ShippedTo", defaultScope)
+    println(s"Generated XML is :: ${renderedXML}")
+  }
+}
